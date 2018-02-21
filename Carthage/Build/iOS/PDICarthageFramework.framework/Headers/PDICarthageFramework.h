5ce7888d055ee2d31f99f0d731e3ef3790fc0e73
Erick Quintanar <erick.torres@idean.com>
2018-02-21 13:29:47 -0800

    Carthage Files pushed

---
+++ Carthage/Build/iOS/PDICarthageFramework.framework/Headers/PDICarthageFramework.h
@@ -0,0 +1,19 @@
+//
+//  PDICarthageFramework.h
+//  PDICarthageFramework
+//
+//  Created by Erick Quintanar on 2/20/18.
+//  Copyright © 2018 Idean. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PDICarthageFramework.
+FOUNDATION_EXPORT double PDICarthageFrameworkVersionNumber;
+
+//! Project version string for PDICarthageFramework.
+FOUNDATION_EXPORT const unsigned char PDICarthageFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PDICarthageFramework/PublicHeader.h>
+
+
